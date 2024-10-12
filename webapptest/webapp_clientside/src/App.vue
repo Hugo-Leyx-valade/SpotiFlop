6e9ab30abad3651652cfe0be644285b647db6b89
Hugo-Leyx-valade <hugo.leyx-valade@efrei.net>
2024-10-13 00:59:52 +0200

    css of the hello page is now working

--- webapptest/webapp_clientside/src/App.vue
+++ webapptest/webapp_clientside/src/App.vue
@@ -23,4 +23,15 @@ export default {
 body{
   background-color: rgb(0,0,0);
 }
+
+@font-face {
+  font-family: 'LilGrotesk-regular'; /* Name your font family */
+  src: url('./assets/Fonts/LilGrotesk-Regular.otf'); /* Locate the .ttf file within your directory*/
+}
+
+@font-face {
+  font-family: 'LilGrotesk-bold'; /* Name your font family */
+  src: url('./assets/Fonts/LilGrotesk-Bold.otf'); /* Locate the .ttf file within your directory*/
+}
+
 </style>
--- webapptest/webapp_clientside/src/components/HelloWorld.vue
+++ webapptest/webapp_clientside/src/components/HelloWorld.vue
@@ -1,66 +1,121 @@
 <template>
   <div class="hello">
-
-    <h1>{{ msg }}</h1>
-    <img id="logo-green" src="../assets/spotyflop.png">
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="/#/authors/list/all"
-          target="_blank"
-        >
-          Authors list
-        </a>
-      </li>
-
-      <li>
-        <a
-          href="/#/users/list/all"
-          target="_blank"
-        >
-          Users List
-        </a>
-      </li>
-
-    </ul>
+  <a href="/#/authors/list/all"><img id="logo-green" src="../assets/spotyflop.png" onmouseover="ChangeLogo(this)"></a>
+  <div class="content" id="txt">
+    <p id="txt1" >{{ msg }}</p>
+    <p id="txt2" >{{ msg }}</p>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to SpotiFlop',
+      msg: 'SpotiFlop',
     }
-  }
+  },
+
+  
+  methods: {
+    changeColor(event) {
+      event.target.src = require('@/assets/spotify-playlist_white.png');
+      document.getElementById("txt1").style.webkitTextStroke = "0.5px white"; 
+      document.getElementById("txt2").style.color = "white";
+    },
+
+    revertColor(event) {
+      event.target.src = require('@/assets/spotyflop.png');
+      document.getElementById("txt1").style.webkitTextStroke = "0.5px #1bc156"; 
+      document.getElementById("txt2").style.color = "#1bc156";
+    }
+
+  },
+
+
+  mounted() {
+    // Add event listeners when the component is mounted
+    const logo = document.getElementById('logo-green');
+    if (logo) {
+      logo.addEventListener('mouseover', this.changeColor);
+      logo.addEventListener('mouseout', this.revertColor);
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
+
+@import url("https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900");
+
+* {
+  margin: 0;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
 }
 
-#logo-green{
-  position: fixed;
-  top: 0;
-  left: 0;
-  scale: 40%;
-  transition: all 0.3s;
+body {
+  display: flex;
+  background: #000;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
 }
 
+.content {
+  position: relative;
+}
+
+
+.content p {
+  color: #fff;
+  font-size: 8em;
+  position: absolute;
+  transform:translateX(149%);
+}
+
+.content p:nth-child(1) {
+  color: transparent;
+  -webkit-text-stroke: 0.5px #1bc156;
+}
+
+.content p:nth-child(2) {
+  color: #1bc156;
+  animation: animate 4s ease-in-out infinite;
+}
+
+
+@keyframes animate {
+  0%,
+  100% {
+    clip-path: polygon(
+      0% 45%,
+      16% 44%,
+      33% 50%,
+      54% 60%,
+      70% 61%,
+      84% 59%,
+      100% 52%,
+      100% 100%,
+      0% 100%
+    );
+  }
+
+  50% {
+    clip-path: polygon(
+      0% 60%,
+      15% 65%,
+      34% 66%,
+      51% 62%,
+      67% 50%,
+      84% 45%,
+      100% 46%,
+      100% 100%,
+      0% 100%
+    );
+  }
+}
 </style>
