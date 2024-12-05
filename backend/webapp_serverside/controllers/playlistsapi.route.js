1431dd187b852e29ddf1373d27e490d6e0a807a2
Hugo-Leyx-valade <hugo.leyx-valade@efrei.net>
2024-12-05 23:07:49 +0100

    playlist backend data handling

--- backend/webapp_serverside/controllers/playlistsapi.route.js
+++ backend/webapp_serverside/controllers/playlistsapi.route.js
@@ -14,24 +14,37 @@ async function playlistListAction(request, response) {
     response.send(JSON.stringify(playlists));
 }
 async function playlistShowAction(request, response) {
-    var onePlaylist = await playlistRepo.getOnePlaylist(request.params.playlistId);
-    var songsOfPlaylist = await playlistRepo.getPlaylistSongsById(request.params.playlistId);
-    console.log( "caca" + JSON.stringify(songsOfPlaylist ));
-    response.send(JSON.stringify({ "playlist": onePlaylist, "songs": songsOfPlaylist}));
+    try{
+        const errorState = 0;
+        var onePlaylist = await playlistRepo.getOnePlaylist(request.params.playlistId);
+        if(onePlaylist === false){
+            errorState = 1;
+        }
+        var songsOfPlaylist = await playlistRepo.getPlaylistSongsById(request.params.playlistId);
+        console.log( "caca" + JSON.stringify(songsOfPlaylist ));
+        response.send(JSON.stringify({"error":errorState,"object":{ "playlist": onePlaylist, "songs": songsOfPlaylist}}));
+    }catch(error){
+        response.send(JSON.stringify({"error":1,"object":null}));
+    }
 }
 async function playlistDelAction(request, response) {
     // TODO: first remove extras for car, unless the car cannot be removed!!!
-    var numRows = await playlistRepo.delOnePlaylist(request.params.playlistId);
-    let result = { rowsDeleted: numRows };
-    response.send(JSON.stringify(result));
+    try{
+        var numRows = await playlistRepo.delOnePlaylist(request.params.playlistId);
+        let result = { rowsDeleted: numRows };
+        response.send(JSON.stringify(result));
+    }catch(error){
+        response.send(JSON.stringify(false));
+    }
 }
 
 
 async function playlistAddAction(request, response) {
     // var json = JSON.stringify(request.body); // bodyParser can process json in body + regular POST form input too
     // console.log(json);    var songId = request.params.songId; 
-    console.log("addOnePlaylist" + JSON.stringify(request.body.title) + JSON.stringify(request.body.state) + JSON.stringify(request.body._description) + JSON.stringify(request.body.id_user));
-
+    if(request.body.title.length === 0 || request.body.date_of_post.length === 0 || request.body.id_user === 0){
+        response.send(JSON.stringify(false));
+    }else{
     var numRows = await playlistRepo.addOnePlaylist(
             request.body.title, 
             request.body.state, 
@@ -41,21 +54,26 @@ async function playlistAddAction(request, response) {
         let result = { rowsUpdated: numRows };
         response.send(JSON.stringify(result));
     }
+}
 
 async function playlistUpdateAction(request, response) {
     // var json = JSON.stringify(request.body); // bodyParser can process json in body + regular POST form input too
     // console.log(json);
     // TODO: !!! INPUT VALIDATION !!!
     var playlistId = request.params.playlistId;
-    var numRows = await playlistRepo.editOnePlaylist(
-        request.body.title, 
-        request.body.date_of_post, 
-        request.body.number_of_save, 
-        request.body._description, 
-        request.body.id_playlist
-    );
-    let result = { rowsUpdated: numRows };
-    response.send(JSON.stringify(result));
+    if(request.body.title.length === 0 || request.body.date_of_post.length === 0){
+        response.send(JSON.stringify(false));
+    }else{
+        var numRows = await playlistRepo.editOnePlaylist(
+            request.body.title, 
+            request.body.date_of_post, 
+            request.body.number_of_save, 
+            request.body._description, 
+            request.body.id_playlist
+        );
+        let result = { rowsUpdated: numRows };
+        response.send(JSON.stringify(result));
+    }
 }
 
 module.exports = router;
